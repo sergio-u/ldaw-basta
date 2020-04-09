dadc1286ca2b05fbdfadd7e692f844a30a5c1210
Sergio Ugalde <serch037@gmail.com>
2020-04-09 12:32:56 -0500

    Add basic game functionality.

---
+++ public/js/app.js
@@ -0,0 +1,174 @@
+if (!alertify.myAlert) {
+    //define a new dialog
+    alertify.dialog('myAlert', function() {
+        return {
+            main: function(message) {
+                this.message = message;
+            },
+            setup: function() {
+                return {
+                    buttons: [{
+                        text: "cool!",
+                        key: 27 /*Esc*/
+                    }],
+                    focus: {
+                        element: 0
+                    }
+                };
+            },
+            prepare: function() {
+                this.setContent(this.message);
+            }
+        };
+    });
+}
+
+var socket = null;
+var username = null;
+var gameStatus = ""; // waiting, started, ended
+var startedCountdown = false;
+var isSubmitted = false;
+
+function beginCountDown() {
+    if (!startedCountdown) {
+        startedCountdown = true;
+        disableInputs();
+        $('#countdown').show();
+        count = 10;
+        var x = setInterval(function() {
+            count -= 1;
+            $('#countdown').text("Quedan: " + count + " segundos");
+            if (count <= 0) {
+                clearInterval(x);
+                console.log("Real done");
+                doSubmit();
+            }
+        }, 1000);
+    }
+}
+
+
+function connectToSocketIo() {
+    socket = io();
+
+    socket.on('setup', function(data) {
+
+        console.log(data);
+        console.log(socket.id);
+        socket.emit('confirm-setup');
+        username = data.username;
+        $("#username").text("Jugador: " + username);
+        $("#page-loader").hide();
+        $("#main-screen").show();
+
+        // Testing
+        $("#main-screen").show();
+        $("#game-screen").show();
+    });
+
+    socket.on('test', function(data) {
+        console.log("test");
+        console.log(data);
+    });
+
+
+    socket.on('begin-countdown', function() {
+        beginCountDown();
+    });
+
+
+    socket.on('waiting-game', function() {
+        console.log("waiting-game");
+        $("#game-loader").show();
+    });
+
+  socket.on('game-results', function(data) {
+    console.log("game-results");
+    console.log(data);
+    askIfContinue();
+  });
+
+    socket.on('waiting-players', function() {
+        $("#player-loader").show();
+        console.log("waiting-players");
+    });
+
+    socket.on('start-game', function(data) {
+        console.log("start-game");
+        console.log(data);
+        $("#game-screen").show();
+        $("#current-letter").text("Letra actual: " + data);
+        $("#game-loader").hide();
+        $("#player-loader").hide();
+    });
+}
+
+$(".input").on("change keypress paste focus textInput input", function() {
+    if ($('#nombre').val() !== '' &&
+        $('#color').val() !== '' &&
+        $('#fruto').val() != '') {
+        $('#submit').prop('disabled', false);
+    } else {
+        $('#submit').prop('disabled', true);
+    }
+});
+
+let continuePlaying = true;
+function askIfContinue(){
+  // Do Stuff
+  if (continuePlaying) {
+    resetState();
+  } else {
+    doDisconnect();
+  }
+}
+
+function doDisconnect(){
+  console.log("Do Disconnect");
+}
+
+function resetState(){
+  console.log("Reset State");
+}
+
+function handleSubmit(e) {
+    console.log(e);
+    console.log("Submit");
+    e.preventDefault();
+    doSubmit();
+}
+
+function doSubmit() {
+    if (!isSubmitted) {
+        let userAnswer = {};
+        userAnswer['nombre'] = $('#nombre').val();
+        userAnswer['color'] = $('#color').val();
+        userAnswer['fruto'] = $('#fruto').val();
+        console.log(userAnswer);
+        socket.emit('game-submit', userAnswer);
+        isSubmitted = true;
+        beginCountDown();
+    }
+}
+
+function disableInputs() {
+    toggleInputs(true);
+}
+
+function enableInputs() {
+    toggleInputs(false);
+}
+
+function toggleInputs(value) {
+    $('#nombre').prop("disabled", value);
+    $('#color').prop("disabled", value);
+    $('#fruto').prop("disabled", value);
+    $('#submit').prop("disabled", value);
+}
+
+$('#user-answers').submit(handleSubmit);
+
+$(function() {
+    connectToSocketIo();
+    //alertify.myAlert("Browser dialogs made easy!");
+});
--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const webRoutes = require('./routes/web');
 // Session imports
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
-let flash = require('express-flash');
-let passport = require('passport');
 
 // Express app creation
 const app = express();
@@ -21,8 +19,8 @@ const hbshelpers = require("handlebars-helpers");
 const multihelpers = hbshelpers();
 const extNameHbs = 'hbs';
 const hbs = exphbs.create({
-  extname: extNameHbs,
-  helpers: multihelpers
+    extname: extNameHbs,
+    helpers: multihelpers
 });
 app.engine(extNameHbs, hbs.engine);
 app.set('view engine', extNameHbs);
@@ -31,26 +29,196 @@ app.set('view engine', extNameHbs);
 let sessionStore = new session.MemoryStore;
 app.use(cookieParser());
 app.use(session({
-  cookie: { maxAge: 60000 },
-  store: sessionStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: appConfig.secret
+    cookie: {
+        maxAge: 60000
+    },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: appConfig.secret
 }));
-app.use(flash());
 
-// Configuraciones de passport
-require('./configs/passport');
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Receive parameters from the Form requests
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 // Routes
 app.use('/', webRoutes);
+app.use('/', express.static(__dirname + '/public'));
+
+// Socket.io
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // App init
-app.listen(appConfig.expressPort, () => {
-  console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+server.listen(appConfig.expressPort, () => {
+    console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+});
+
+randomChar = () => {
+    const min = 65;
+    const max = 90;
+    randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    return String.fromCharCode(randomInt);
+}
+
+
+var emoji = require('node-emoji');
+let playerCount = 0;
+let gameLock = false;
+const minPlayers = 2;
+let firstSubmission = false;
+let players = {};
+let submissions = {};
+io.on('connection', function(socket) {
+    const sessionID = socket.id;
+    const username = emoji.random();
+    console.log(username.emoji);
+
+    /*
+    setTimeout(() => {
+        console.log("disconnect");
+        console.log(socket.rooms);
+        playeCount--;
+        socket.disconnect(true);
+        delete players.sessionID;
+    }, 10000);
+*/
+
+    const joinWaitingRoom = () => {
+        socket.join('waiting-room');
+        players[sessionID] = socket;
+
+        if (playerCount < minPlayers) {
+            console.log("Waiting players");
+            console.log(sessionID);
+            //io.sockets.to(sessionID).emit('waiting-players', 'this is only for you');
+            socket.emit('waiting-players');
+        }
+
+        if (playerCount >= minPlayers) {
+            startGame();
+        }
+    };
+
+    const startGame = () => {
+        if (!gameLock) {
+            console.log("Start game");
+            console.log("Player Count" + Object.keys(players).length);
+            io.to('waiting-room').emit('test', 'kicking out');
+            for (let key in players) {
+                players[key].leave('waiting-room');
+                players[key].join('game');
+            }
+            currChar = randomChar();
+            console.log(currChar);
+            io.to('game').emit('start-game', currChar);
+            gameLock = true;
+        } else {
+            console.log("Waiting Game");
+            socket.emit('waiting-game');
+        }
+    };
+
+    socket.on('say to someone', function(id, msg) {
+        socket.broadcast.to(id).emit('my message', msg);
+    });
+
+    socket.on('game-submit', function(data) {
+        console.log(data);
+        submissions[sessionID] = data;
+        submitCount = Object.keys(submissions).length;
+        if (submitCount == 1) {
+            io.to('game').emit('begin-countdown');
+            firstSubmission = true;
+        }
+        roomCount = io.sockets.adapter.rooms['game'].length;
+        console.log("roomCount");
+        if (submitCount == roomCount) {
+            gradeSubmission(submissions);
+        }
+    });
+
+    socket.on('reset-player', () => {
+        console.log("Reset Player");
+    });
+
+    socket.on('cancel-player', () => {
+        console.log("Cancel Player");
+    });
+
+    socket.emit('setup', {
+        'username': `${username.emoji}`
+    });
+
+    socket.on('confirm-setup', () => {
+        console.log("Confirm Setup");
+        playerCount++;
+        joinWaitingRoom();
+    });
+    const endGame =
+        const gradeSubmission = (submissions) => {
+            grades = {}
+            countwords = {}
+            console.log("Grading submission");
+
+            // Count occurrences
+            for (let key in submissions) {
+                if (key["nombre"] !== "") {
+
+                    if (countwords["nombre"][key["nombre"]]) {
+                        countwords["nombre"][key["nombre"]]++;
+                    } else {
+                        countwords["nombre"][key["nombre"]] = 0;
+                    }
+                }
+
+                if (key["color"] !== "") {
+                    if (countwords["color"][key["color"]]) {
+                        countwords["color"][key["color"]]++;
+                    } else {
+                        countwords["color"][key["color"]] = 0;
+                    }
+                }
+
+                if (key["fruto"] !== "") {
+                    if (countwords["fruto"][key["fruto"]]) {
+                        countwords["fruto"][key["fruto"]]++;
+                    } else {
+                        countwords["fruto"][key["fruto"]] = 0;
+                    }
+                }
+            }
+            // Grade
+            for (let key in submissions) {
+                grades[key] = 0;
+                if (key["nombre"] !== "") {
+                    grades[key] += 100 / countwords["nombre"][key["fruto"]];
+                }
+                if (key["color"] !== "") {
+                    grades[key] += 100 / countwords["color"][key["fruto"]];
+                }
+                if (key["fruto"] !== "") {
+                    grades[key] += 100 / countwords["fruto"][key["fruto"]];
+                }
+            }
+            console.log("Graded");
+            console.log(grades);
+            // Get max
+            test = Object.keys(object).filter(x => {
+                return object[x] == Math.max.apply(null,
+                    Object.values(object));
+            });
+            test = Object.keys(grades).reduce((a, b) => grades[a] > grades[b] ? grades[a] : grades[b]);
+            for (let key in submissions) {
+                if (grades[key] === test) {
+                    io.sockets.to(sessionID).emit('game-results', true);
+                } else {
+                    io.sockets.to(sessionID).emit('game-results', false);
+                }
+            }
+
+        };
 });
